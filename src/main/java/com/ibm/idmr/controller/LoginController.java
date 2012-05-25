3009e642ff168da43d75dee783b41f6335c938ef
unknown <jurijspe@T61WorkLaptop.(none)>
2012-05-25 11:30:09 +0100

    Added LoginController

---
+++ src/main/java/com/ibm/idmr/controller/LoginController.java
@@ -0,0 +1,20 @@
+package com.ibm.idmr.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+    
+    @RequestMapping({"/","/index"})
+    public String showIndex() {
+        System.out.println("In the Controller. Index");
+        return "index";
+    }
+    
+    @RequestMapping("/login")
+    public String showLogin() {
+        System.out.println("In the Controller. Login");
+        return "login";
+    }
+}
