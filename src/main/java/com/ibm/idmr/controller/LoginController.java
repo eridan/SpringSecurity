6b18c5783eaf63eec267db56877513a395c8c076
unknown <jurijspe@T61WorkLaptop.(none)>
2012-05-29 13:47:05 +0100

    Added Login controller and login page reference to http_security

--- src/main/java/com/ibm/idmr/controller/LoginController.java
+++ src/main/java/com/ibm/idmr/controller/LoginController.java
@@ -1,20 +1,43 @@
 package com.ibm.idmr.controller;
 
+import java.security.Principal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginController {
-    
-    @RequestMapping({"/","/index"})
-    public String showIndex() {
-        System.out.println("In the Controller. Index");
-        return "index";
+
+    @RequestMapping(value = "/welcome", method = RequestMethod.GET)
+    public String printWelcome(ModelMap model, Principal principal) {
+        String name = principal.getName();
+        model.addAttribute("username", name);
+        model.addAttribute("message", "Spring Security Custom Form example");
+        return "hello";
+    }
+
+    @RequestMapping(value = "/login")
+    public String login(ModelMap model) {
+        System.out.println("LoginController. Login method");
+        return "login";
+    }
+
+    @RequestMapping(value = "/loginfailed")
+    public String loginerror(ModelMap model) {
+        System.out.println("Login controller. Login Failed");
+        model.addAttribute("error", "true");
+        return "login";
+    }
+
+    @RequestMapping(value = "/logout")
+    public String logout(ModelMap model) {
+        System.out.println("LoginController. Log out.");
+        return "login";
     }
     
-    @RequestMapping("/login")
-    public String showLogin() {
-        System.out.println("In the Controller. Login");
-        return "login";
+     @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String indexPage () {
+        return "index";
     }
 }
--- src/main/webapp/WEB-INF/jsp/index.jsp
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -2,6 +2,8 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
     "http://www.w3.org/TR/html4/loose.dtd">
 
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
@@ -15,5 +17,10 @@
                 the redirection in</i> <tt>redirect.jsp</tt> <i>to point to the new
                 welcome page and also update the welcome-file setting in</i>
             <tt>web.xml</tt>.</p>
+        
+        <h3>Message : ${message}</h3>	
+	<h3>Username : ${username}</h3>	
+ 
+	<a href="<c:url value="/j_spring_security_logout" />" > Logout</a>
     </body>
 </html>
